5356fed55da00f405ef67c0dcc08e15091ecf06e
yotam-biu <nagaryo3@biu.ac.il>
2024-11-16 09:48:02 +0200

    Create edge_case_test.py

---
+++ .tests/edge_case_test.py
@@ -0,0 +1,5 @@
+from edge_case_exercise import edge_case_exercise.ipynb
+
+def test_edge_case():
+    print("start the test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    assert move([0,0,0,1,0,0,0], "left") ==  [0,0,1,0,0,0,0]
