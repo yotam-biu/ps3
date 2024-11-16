28c67288100d07b7bc470fe772c601be090246cd
yotam-biu <nagaryo3@biu.ac.il>
2024-11-16 16:44:07 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,2 @@
 def approximate_pi(n_terms):
-
-    ls = []
-    for i in range(n_terms):
-        ls.append((-1)**i/(i*2 + 1))
-    return 4 * sum(ls)# replace pass with your code
+    pass # replace pass with your code
