ed6eff7e348d9b99675c68505f0e26326035ee19
yotam-biu <nagaryo3@biu.ac.il>
2024-11-16 10:14:59 +0200

    Update leibniz_series_test.py

--- .tests/leibniz_series_test.py
+++ .tests/leibniz_series_test.py
@@ -1,4 +1,8 @@
 from leibniz_series_exercise import approximate_pi
 
 def test_leibniz_series():
-    assert True
+    assert abs(approximate_pi(1) - 4) < 0.1, "Test failed: approximate_pi(1) should be close to 4"
+    assert abs(approximate_pi(5) - 3.339) < 0.01, "Test failed: approximate_pi(5) should be close to 3.339."
+    assert abs(approximate_pi(99) - 3.151) < 0.01, "Test failed: approximate_pi(99)."
+    assert abs(approximate_pi(999) - 3.1425) < 0.001, "Test failed: approximate_pi(999)."
+
