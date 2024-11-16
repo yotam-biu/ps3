2ef26357925687554499da13455c52f677fedd0a
yotam-biu <nagaryo3@biu.ac.il>
2024-11-16 10:24:52 +0200

    Update edge_case_test.py

--- .tests/edge_case_test.py
+++ .tests/edge_case_test.py
@@ -1,5 +1,16 @@
 from edge_case_exercise import move
 
 def test_edge_case():
-    print("start the test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    assert move([0,1,0], "left") ==  [1,0,0]
+    assert move([0,1,0], "right") ==  [0,0,1]
     assert move([0,0,0,1,0,0,0], "left") ==  [0,0,1,0,0,0,0]
+    assert move([1,0,0,0,0,0,0], "right") ==  [0,1,0,0,0,0,0]
+    assert move([1,0,0,0,0,0,0], "left") ==  [1,0,0,0,0,0,0]
+    assert move([0,0,0,0,0,0,1], "left") ==  [0,0,0,0,0,1,0]
+    assert move([0,0,0,0,0,0,1], "right") ==  [0,0,0,0,0,0,1]
+    assert move([1,0], "left") ==  [1,0]
+    assert move([0,1], "left") ==  [1,0]
+    assert move([1,0], "right") ==  [0,1]
+    assert move([0,1], "right") ==  [0,1]
+    assert move([1], "right") ==  [1]
+    assert move([1], "left") ==  [1]
